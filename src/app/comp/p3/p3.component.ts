0074362758e2b5aff77b43bd1795c22f9c846f6b
psink-code <113106328+psink-code@users.noreply.github.com>
2023-09-01 07:25:47 +0200

    ciao

--- src/app/comp/p3/p3.component.ts
+++ src/app/comp/p3/p3.component.ts
@@ -11,46 +11,44 @@ export class P3Component {
   piatti: any = [];
   num: number = -1;
   cart: any;
+  currentId: number = 0;
+
   generate() {
     for (let i = 0; i < 100; i++) {
       this.api
         .apiepic(`https://www.themealdb.com/api/json/v1/1/random.php`)
         .subscribe((data: any) => {
-          this.piatti[i] = {
+          const nuovoPiatto = {
             name: data.meals[0].strMeal,
             id: data.meals[0].idMeal,
             img: data.meals[0].strMealThumb,
             dess: data.meals[0].strInstructions,
             ordini: 1,
+            ids: Date.now(),
           };
+          if (!this.piatti.some((p: any) => p.id === nuovoPiatto.id)) {
+            this.piatti[i] = nuovoPiatto;
+          }
         });
     }
     return this.piatti;
   }
   counter() {}
   add(piatto: any) {
+    const uniqueId = Date.now();
+    this.api.carrello[this.num] = {
+      ...piatto,
+      ids: uniqueId,
+    };
     this.num++;
-    let tr: any = [];
-    tr = this.api.carrello.map((data: any) => {
-      if (data.id == piatto.id) {
-        Number(data.ordini) + data.ordini;
-        console.log(data.ordini);
-
-        return true;
-      } else return false;
-    });
-    if (tr != true) {
-      this.api.carrello[this.num] = piatto;
-    }
-    tr = [];
-    console.log(tr);
-    console.log(this.api.carrello[this.num]);
+    console.log(this.api.carrello);
   }
   kik(piatto: any) {
     this.api.carrello = this.api.carrello.filter(
-      (data: any) => data.id != piatto.id
+      (data: any) => data.ids != piatto.ids
     );
     console.log(this.api.carrello);
+    this.num--;
     return this.api.carrello;
   }
   constructor(public api: ApiService, private router: Router) {}
